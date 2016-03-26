6fa973d8321a7a628bbeee690a81a231efccad58
Sheldon Woodward <sheldonkwoodward@gamil.com>
2016-03-26 12:49:48 -0700

    Added input functions and more cursor functions

--- TTT-2.0/TTT-2.0/SWLib.h
+++ TTT-2.0/TTT-2.0/SWLib.h
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <limits>
 using namespace std;
 
 namespace sw {
@@ -16,9 +17,17 @@ namespace sw {
 		class Cursor {
 		public:
 			static void resetPos();
+			static COORD getPos();
 			static void setPos(COORD coord);
 			static void clearLine();
+			static void backLine();
+			static void backLine(int numLines);
 			static void clearBelow();
 		};
+	};
+	class Input {
+	public:
+		static int integer();
+		static int integer(int min, int max);
 	};
 }
